6ce81866f4551be041243b56e4d81e41a33c4cb9
xoma_star <sasha.sirotyin09@gmail.com>
2023-01-12 19:37:20 +0500

    123

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -43,13 +43,14 @@ const Map = ({sector, setCoordinates}: props) => {
                     if(sector[0].length === 3 && sector[1].length === 3) {
                         const a = `${sector[0]}${j.toString(16)}:${sector[1]}${i.toString(16)}`.toUpperCase()
                         tg.MainButton.setParams({
-                            text: `Информация о системе (${a})`
+                            text: `Скопировать координаты (${a})`
                         })
                         if(x !== 'HIDE'){
                             tg.MainButton.show()
                             tg.MainButton.onClick = () => {
-                                tg.sendData(JSON.stringify({command: 'travel', coordinates: a}))
+                                // tg.sendData(JSON.stringify({command: 'travel', coordinates: a}))
                                 // tg.close()
+                                navigator.clipboard.writeText(a)
                             }
                         }
                         else{
